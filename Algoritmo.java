77ded909ae8edc3e5f0ff261c004843afd8014a6
Eduardo André Soares <eduardo.andre.soares@icloud.com>
2020-10-23 08:30:55 -0300

    Upando Nomes.txt

--- Algoritmo.java
+++ Algoritmo.java
@@ -3,67 +3,65 @@
  */
 public class Algoritmo {
 
-    public void escreverResultado(int tabuleiro[][]) {
-        for (int i = 0; i < tabuleiro.length; i++) {
-            for (int j = 0; j < tabuleiro.length; j++) {
-                System.out.print(" " + tabuleiro[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public boolean verificarPossibilidades(int tabuleiro[][], int linha, int coluna) {
-
-        int i, j;
-
-        for (i = 0; i < coluna; i++) {
-            if (tabuleiro[linha][i] == 1) {
-                return false;
-            }
-        }
-
-        for (i = linha, j = coluna; i >= 0 && j >= 0; i--, j--) {
-            if (tabuleiro[i][j] == 1) {
-                return false;
-            }
-        }
-
-        for (i = linha, j = coluna; j >= 0 && i < tabuleiro.length; i++, j--) {
-            if (tabuleiro[i][j] == 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean resolverNRainhasRecursivo(int tabuleiro[][], int coluna) {
-        if (coluna >= tabuleiro.length) {
+    public boolean resolverNRainhasRecursivo(int xadrez[][], int coluna) {
+        if (coluna >= xadrez.length) {
             return true;
         }
 
-        for (int i = 0; i < tabuleiro.length; i++) {
-            if (verificarPossibilidades(tabuleiro, i, coluna)) {
-                tabuleiro[i][coluna] = 1;
+        for (int i = 0; i < xadrez.length; i++) {
+            if (verificarPossibilidades(xadrez, i, coluna)) {
+                xadrez[i][coluna] = 1;
 
-                if (resolverNRainhasRecursivo(tabuleiro, coluna + 1) == true) {
+                if (resolverNRainhasRecursivo(xadrez, coluna + 1) == true) {
                     return true;
                 }
-                tabuleiro[i][coluna] = 0;
+                xadrez[i][coluna] = 0;
             }
         }
         return false;
     }
 
-    public boolean resolverNRainhasBT(int tamanho){
-        int tabuleiro[][] = new int[tamanho][tamanho];
+    public boolean resolverNRainhasBT(int tamanho) {
+        int xadrez[][] = new int[tamanho][tamanho];
 
-        if(resolverNRainhasRecursivo(tabuleiro, 0) == false){
+        if (resolverNRainhasRecursivo(xadrez, 0) == false) {
             System.out.println("Não existe esta solução");
             return false;
         }
 
-        escreverResultado(tabuleiro);
+        escreverResultado(xadrez);
         return true;
     }
 
+    public boolean verificarPossibilidades(int xadrez[][], int linha, int coluna) {
+        int i, j;
+
+        for (i = 0; i < coluna; i++) {
+            if (xadrez[linha][i] == 1) {
+                return false;
+            }
+        }
+
+        for (i = linha, j = coluna; i >= 0 && j >= 0; i--, j--) {
+            if (xadrez[i][j] == 1) {
+                return false;
+            }
+        }
+
+        for (i = linha, j = coluna; j >= 0 && i < xadrez.length; i++, j--) {
+            if (xadrez[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void escreverResultado(int xadrez[][]) {
+        for (int i = 0; i < xadrez.length; i++) {
+            for (int j = 0; j < xadrez.length; j++) {
+                System.out.print(" " + xadrez[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Algoritmo nRainha = new Algoritmo();
-        nRainha.resolverNRainhasBT(5);
+        nRainha.resolverNRainhasBT(20);
     }
 }
